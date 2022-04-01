f3888e9d518b9c21c1d2b34d23ba9f8c624ddfa3
Bernhard Hörmann <hoermann.b@gmail.com>
2022-04-02 00:16:50 +0200

    ADD: isHidden

--- resources/views/forms/components/button-field.blade.php
+++ resources/views/forms/components/button-field.blade.php
@@ -1,21 +1,23 @@
-<x-forms::field-wrapper
-    :id="$getId()"
-    :label="$getLabel()"
-    :label-sr-only="$isLabelHidden()"
-    :helper-text="$getHelperText()"
-    :hint="$getHint()"
-    :hint-icon="$getHintIcon()"
-    :state-path="$getStatePath()"
->
-    <div {{ $attributes->merge($getExtraAttributes())->class('filament-forms-placeholder-component') }}>
-        <x-caravel-admin::button
-            x-on:click="{{ $getOnClickJs() }}"
-            tag="a"
-            :size="$getSize()"
-            :href="$getHref()"
-            :color="$getColor()"
-            :target="$isTargetBlank() ? '_blank': ''">
-            {{ $getButtonLabel() }}
-        </x-caravel-admin::button>
-    </div>
-</x-forms::field-wrapper>
+@if(!$isHidden())
+    <x-forms::field-wrapper
+        :id="$getId()"
+        :label="$getLabel()"
+        :label-sr-only="$isLabelHidden()"
+        :helper-text="$getHelperText()"
+        :hint="$getHint()"
+        :hint-icon="$getHintIcon()"
+        :state-path="$getStatePath()"
+    >
+        <div {{ $attributes->merge($getExtraAttributes())->class('filament-forms-placeholder-component') }}>
+            <x-caravel-admin::button
+                x-on:click="{{ $getOnClickJs() }}"
+                tag="a"
+                :size="$getSize()"
+                :href="$getHref()"
+                :color="$getColor()"
+                :target="$isTargetBlank() ? '_blank': ''">
+                {{ $getButtonLabel() }}
+            </x-caravel-admin::button>
+        </div>
+    </x-forms::field-wrapper>
+@endif
